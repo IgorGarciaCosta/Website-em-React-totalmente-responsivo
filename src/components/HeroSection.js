81eda6eaca9e0f6e017aabe340387ff32247e2d1
IgorGarciaCosta <igonildo7@gmail.com>
2020-12-11 23:20:54 -0300

    iniciando home page

---
+++ src/components/HeroSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+//rfce
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <h1>A AVENTURA AGUARDA</h1>
+            <p>O que você está esperando?</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline"
+                    buttonSize="btn--large">
+                    INICIAR
+                </Button>
+
+                <Button className="btns" buttonStyle="btn--primary"
+                    buttonSize="btn--large">
+                    VER TRAILER <i className="fa-play-circle"/>
+                </Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeroSection
